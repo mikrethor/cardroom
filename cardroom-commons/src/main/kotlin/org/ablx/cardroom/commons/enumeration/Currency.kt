8b636114e0cad605364977e9512866311fdbdaef
Xavier BOUCLET <mikrethor@gmail.com>
2017-08-01 18:41:13 -0400

    Adding PLAY_MONEY as a currency

--- cardroom-commons/src/main/kotlin/org/ablx/cardroom/commons/enumeration/Currency.kt
+++ cardroom-commons/src/main/kotlin/org/ablx/cardroom/commons/enumeration/Currency.kt
@@ -3,7 +3,8 @@ package org.ablx.cardroom.commons.enumeration
 enum class Currency(val symbol: String, val shortName: String) {
     EURO("\u20AC", "EUR"),
     USD("\u0024", "USD"),
-    POUND("\u00A3", "GBP")
+    POUND("\u00A3", "GBP"),
+    PLAY_MONEY("", "PLAY_MONEY")
 
 
 }
