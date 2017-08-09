ab6bfa6882d2c8de0ee3cc1073e69969d3aaa2c9
Xavier BOUCLET <mikrethor@gmail.com>
2017-08-08 20:11:23 -0400

    Initial spring boot project

---
+++ cardroom-backend/src/main/kotlin/org/ablx/cardroom/cardroombackend/CardroomBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.ablx.cardroom.cardroombackend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CardroomBackendApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CardroomBackendApplication::class.java, *args)
+}
---
+++ cardroom-backend/src/test/kotlin/org/ablx/cardroom/cardroombackend/CardroomBackendApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.ablx.cardroom.cardroombackend
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CardroomBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
