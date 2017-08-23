d0acd51e6c2313f7d2b6e5a6e9e8f14a76f4d9be
Xavier BOUCLET <mikrethor@gmail.com>
2017-08-22 23:43:50 -0400

    Adding javafx fxml template

--- cardroom-client/src/main/kotlin/org/ablx/cardroom/client/Main.kt
+++ cardroom-client/src/main/kotlin/org/ablx/cardroom/client/Main.kt
@@ -7,27 +7,46 @@ import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.layout.StackPane
 import javafx.stage.Stage
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.fxml.FXML
+import javafx.scene.text.Text
 
 
 class Main : Application() {
 
+    @FXML private val actiontarget: Text? = null
+
+    @FXML protected fun handleSubmitButtonAction(event: ActionEvent) {
+        actiontarget?.setText("Sign in button pressed")
+    }
+
+    public val action: EventHandler<ActionEvent> = object : EventHandler<ActionEvent> {
+
+        override fun handle(event: ActionEvent) {
+            println("Hello World!")
+        }
+    }
 
     override fun start(primaryStage: Stage) {
-        val btn = Button()
-        btn.setText("Say 'Hello World'")
-        btn.setOnAction(object : EventHandler<ActionEvent> {
 
-            override fun handle(event: ActionEvent) {
-                println("Hello World!")
-            }
-        })
+//        val btn = Button()
+//        btn.setText("Say 'Hello World'")
+//        val action: EventHandler<ActionEvent> = object : EventHandler<ActionEvent> {
+//
+//            override fun handle(event: ActionEvent) {
+//                println("Hello World!")
+//            }
+//        }
+//        btn.setOnAction(action)
 
-        val root = StackPane()
-        root.children.add(btn)
+        //val root = StackPane()
+      val root: Parent = FXMLLoader.load<Any>(javaClass.getResource("/cardroom.fxml")) as Parent
+    //    root.children.add(btn)
 
         val scene = Scene(root, 300.0, 250.0)
 
-        primaryStage.setTitle("Hello World!")
+        primaryStage.setTitle("Cardroom")
         primaryStage.setScene(scene)
         primaryStage.show()
     }
@@ -37,7 +56,7 @@ class Main : Application() {
         fun main(args: Array<String>) {
 
             Application.launch(Main::class.java, *args)
-           
+
 
         }
     }
