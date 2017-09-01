4f1ea9d85b80cbaf4250e98dc8ec08463084bfb1
Xavier BOUCLET <mikrethor@gmail.com>
2017-09-01 17:16:23 -0400

    Upgrading kotlin version to 1.1.4-3

--- cardroom-commons/src/test/kotlin/org/ablx/cardroom/commons/data/HandTest.kt
+++ cardroom-commons/src/test/kotlin/org/ablx/cardroom/commons/data/HandTest.kt
@@ -8,8 +8,7 @@ import kotlin.test.assertEquals
 class HandTest {
     @Test
     fun testHandInit() {
-        val cardroom = Cardroom(1, Operator.WINAMAX, Domain.COM, "url")
-        val player = Player(1, "dd", cardroom)
+
         val hand = Hand("handid")
         assertEquals("handid", hand.cardroomHandId)
 
